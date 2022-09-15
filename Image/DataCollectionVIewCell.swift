a3fce017637f4d9611d143fa796d283e79937545
Anton Pavlov <toni.pavlov.85@bk.ru>
2022-09-15 16:32:19 +0300

    New commit

---
+++ Image/DataCollectionVIewCell.swift
@@ -0,0 +1,13 @@
+//
+//  DataCollectionVIewCell.swift
+//  Image
+//
+//  Created by Антон Павлов on 15.09.2022.
+//
+
+import UIKit
+
+class DataCollectionVIewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var img: UIImageView!
+}
